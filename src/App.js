14d4a95d6e9243570a45dc456c8e69f9619cf5cd
amandena <manda.panda91@yahoo.com>
2020-06-04 22:59:33 -0400

    imported new components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import './App.css'
 import Header from './components/Header'
+import Movie from './components/Movie'
+import Search from './components/Search'
 
 function App() {
   return (
     <div className="App">
       App
       <Header/>
+      <Search/>
+      <Movie/>
     </div>
   )
 }
