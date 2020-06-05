ec46983a32a190952f832b0035a114f0887eb91a
amandena <manda.panda91@yahoo.com>
2020-06-04 23:08:59 -0400

    added custom movie api key and imported useState and useEffect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css'
 import Header from './components/Header'
 import Movie from './components/Movie'
 import Search from './components/Search'
+
+const MOVIE_API_URL = 'https://www.omdbapi.com/?s=man&apikey=d681ac61'
 
 function App() {
   return (
