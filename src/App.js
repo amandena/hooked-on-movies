9948dca73c21514b5d3fb2e269e45dc40f664f31
amandena <manda.panda91@yahoo.com>
2020-06-05 21:19:31 -0400

    added useState and useEffect hooks

--- src/App.js
+++ src/App.js
@@ -7,6 +7,19 @@ import Search from './components/Search'
 const MOVIE_API_URL = 'https://www.omdbapi.com/?s=man&apikey=d681ac61'
 
 function App() {
+  const [loading, setLoading] = useState(true)
+  const [movies, setMovies] = useState([])
+  const [errorMessage, setErrorMessage] = useState(null)
+
+  useEffect(() => {
+    fetch(MOVIE_API_URL)
+      .then(response => response.json())
+      .then(jsonResponse => {
+        setMovies(jsonResponse.Search)
+        setLoading(false)
+      })
+  },[])
+
   return (
     <div className="App">
       App
